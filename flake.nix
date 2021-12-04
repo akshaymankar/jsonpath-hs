710f5904b0ccc0cba5f1c18f2babdaff0f774ea1
Akshay Mankar <itsakshaymankar@gmail.com>
2021-12-04 23:01:00 +0100

    flake.nix: Add envs for CI

--- flake.nix
+++ flake.nix
@@ -9,20 +9,48 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in rec {
-        packages.dev-env = pkgs.buildEnv {
-          name = "jsonpath-hs-dev";
-          paths = [
-            # Tools
-            pkgs.haskell.compiler.ghc901
-            pkgs.haskellPackages.cabal-install
-            pkgs.gnumake
-            (pkgs.haskell-language-server.override {supportedGhcVersions = ["901"];})
-            pkgs.haskellPackages.implicit-hie
+        packages = {
+          dev-env = pkgs.buildEnv {
+            name = "jsonpath-hs-dev";
+            paths = [
+              # Tools
+              pkgs.haskell.compiler.ghc901
+              pkgs.haskellPackages.cabal-install
+              (pkgs.haskell-language-server.override {supportedGhcVersions = ["901"];})
+              pkgs.haskellPackages.implicit-hie
 
-            # For cabal
-            pkgs.pkgconfig
-            pkgs.binutils
-          ];
+              # For cabal
+              pkgs.pkgconfig
+              pkgs.binutils
+            ];
+          };
+          ci-env-ghc901 = pkgs.buildEnv {
+            name = "jsonpath-hs-ci";
+            paths = [
+              pkgs.haskell.compiler.ghc901
+              pkgs.haskellPackages.cabal-install
+              pkgs.pkgconfig
+              pkgs.binutils
+            ];
+          };
+          ci-env-ghc8107 = pkgs.buildEnv {
+            name = "jsonpath-hs-ci";
+            paths = [
+              pkgs.haskell.compiler.ghc8107
+              pkgs.haskellPackages.cabal-install
+              pkgs.pkgconfig
+              pkgs.binutils
+            ];
+          };
+          ci-env-ghc884 = pkgs.buildEnv {
+            name = "jsonpath-hs-ci";
+            paths = [
+              pkgs.haskell.compiler.ghc884
+              pkgs.haskellPackages.cabal-install
+              pkgs.pkgconfig
+              pkgs.binutils
+            ];
+          };
         };
         defaultPackage = packages.dev-env;
     });
