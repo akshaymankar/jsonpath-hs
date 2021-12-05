0e4d922ce213214494928fea0a1315ac303068e8
Akshay Mankar <itsakshaymankar@gmail.com>
2021-12-05 17:18:48 +0100

    Remove ci-env-* targets

--- flake.nix
+++ flake.nix
@@ -27,36 +27,6 @@
           jsonpath-ghc901 = pkgs.haskell.packages.ghc901.callPackage ./default.nix {};
           jsonpath-ghc8107 = pkgs.haskell.packages.ghc8107.callPackage ./default.nix {};
           jsonpath-ghc884 = pkgs.haskell.packages.ghc884.callPackage ./default.nix {};
-          ci-env-ghc901 = pkgs.buildEnv {
-            name = "jsonpath-hs-ci";
-            paths = [
-              pkgs.haskell.compiler.ghc901
-              pkgs.haskellPackages.cabal-install
-              pkgs.wget
-              pkgs.pkgconfig
-              pkgs.binutils
-            ];
-          };
-          ci-env-ghc8107 = pkgs.buildEnv {
-            name = "jsonpath-hs-ci";
-            paths = [
-              pkgs.haskell.compiler.ghc8107
-              pkgs.haskellPackages.cabal-install
-              pkgs.wget
-              pkgs.pkgconfig
-              pkgs.binutils
-            ];
-          };
-          ci-env-ghc884 = pkgs.buildEnv {
-            name = "jsonpath-hs-ci";
-            paths = [
-              pkgs.haskell.compiler.ghc884
-              pkgs.haskellPackages.cabal-install
-              pkgs.wget
-              pkgs.pkgconfig
-              pkgs.binutils
-            ];
-          };
         };
         defaultPackage = packages.dev-env;
     });
