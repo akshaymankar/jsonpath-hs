4c32b119d076385ed921c79618f41acbf9ec0a32
Akshay Mankar <itsakshaymankar@gmail.com>
2022-05-22 09:23:04 +0200

    ci: Remove symlinking of ca-bundle
    The new nixos image already contains the certs

--- ci/compile-and-test.sh
+++ ci/compile-and-test.sh
@@ -8,11 +8,6 @@ readonly ghc=${2:?"Please provide name of the ghc"}
 nix-env -iA nixpkgs.nixFlakes nixpkgs.git nixpkgs.cachix
 echo 'experimental-features = nix-command flakes' >> /etc/nix/nix.conf
 
-
-# TODO: Delete this when next version of nixos/nix image is published.
-mkdir -p /etc/ssl/certs
-ln -s "$NIX_SSL_CERT_FILE" /etc/ssl/certs
-
 cachix use jsonpath-hs
 
 echo "Building jsonpath with ghc=$ghc"
